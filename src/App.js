23563021397a2ebb65be29575b5ba2bc3808b3a0
dyabk <dzmitry.yabkevich@outlook.com>
2021-10-24 19:03:08 -0400

    Completed Exercise 1.1: course information, step1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import React from 'react'
-const App = () => (
+
+const Header = (props) => (
+  <>
+    <h1>{props.title}</h1>
+  </>
+)
+
+const Content = (props) => (
   <div>
-    <p>Hello world</p>
+    <p>
+      {props.string1} {props.number1}
+    </p>
+    <p>
+      {props.string2} {props.number2}
+    </p>
+    <p>
+      {props.string3} {props.number3}
+    </p>
   </div>
 )
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.number1 + props.number2 + props.number3}</p>
+    </>
+  )
+}
+
+const App = () => {
+  // const-definitions
+
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content string1={part1} string2={part2} string3={part3} number1={exercises1} number2={exercises2} number3={exercises3} />
+      <Total number1={exercises1} number2={exercises2} number3={exercises3} />
+    </div>
+  )
+}
 
 export default App
